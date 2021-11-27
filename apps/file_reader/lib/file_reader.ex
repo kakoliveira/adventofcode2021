868bc5cd54cd18a4ca76e83b1f0dcc5bbb280151
Carlos Oliveira <carlos.oliveira@coverflex.com>
2021-11-27 14:40:14 +0000

    [FileReader] fix credo issues

--- apps/file_reader/lib/file_reader.ex
+++ apps/file_reader/lib/file_reader.ex
@@ -3,12 +3,14 @@ defmodule FileReader do
   Tools to handle file reading and parsing
   """
 
+  @spec read(binary) :: [binary]
   def read(file_path) when is_bitstring(file_path) do
     file_path
     |> File.read!()
     |> String.split("\n")
   end
 
+  @spec clean_content(list) :: list
   def clean_content(content) when is_list(content) do
     Enum.reject(content, &(&1 in [nil, "", "\n"]))
   end
