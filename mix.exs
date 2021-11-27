463afbe71c67c6cdee2ffef3e8692940263f253f
Carlos Oliveira <carlos.oliveira@coverflex.com>
2021-11-27 13:38:54 +0000

    Add ExCoveralls

--- mix.exs
+++ mix.exs
@@ -6,7 +6,16 @@ defmodule AdventOfCode2021.MixProject do
       apps_path: "apps",
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "test.coverage": :test
+      ]
     ]
   end
 
@@ -16,6 +25,16 @@ defmodule AdventOfCode2021.MixProject do
   #
   # Run "mix help deps" for examples and options.
   defp deps do
-    []
+    [
+      {:excoveralls, "~> 0.14", only: :test}
+    ]
+  end
+
+  defp aliases do
+    [
+      "test.coverage": [
+        "coveralls.html"
+      ]
+    ]
   end
 end
