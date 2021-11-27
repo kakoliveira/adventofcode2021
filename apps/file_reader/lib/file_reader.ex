c53fb7d151ebb67403536d8d170e21026ca715eb
Carlos Oliveira <carlos.oliveira@coverflex.com>
2021-11-27 13:02:22 +0000

    FileReader app

---
+++ apps/file_reader/lib/file_reader.ex
@@ -0,0 +1,15 @@
+defmodule FileReader do
+  @moduledoc """
+  Tools to handle file reading and parsing
+  """
+
+  def read(file_path) when is_bitstring(file_path) do
+    file_path
+    |> File.read!()
+    |> String.split("\n")
+  end
+
+  def clean_content(content) when is_list(content) do
+    Enum.reject(content, &(&1 in [nil, "", "\n"]))
+  end
+end
