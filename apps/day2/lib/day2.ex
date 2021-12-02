ecec3b6346b8ce3201fdfecd21a336dd3d09b977
Carlos Oliveira <carlos.oliveira@coverflex.com>
2021-12-02 16:16:59 +0000

    Day2 part 2 solution

--- apps/day2/lib/day2.ex
+++ apps/day2/lib/day2.ex
@@ -19,19 +19,29 @@ defmodule Day2 do
     calculate_position(commands)
   end
 
+  def solve(part: 2, commands: commands) do
+    calculate_position(commands, with_aim: true)
+  end
+
   defp read_commands(file_path) do
     file_path
     |> FileReader.read()
     |> FileReader.clean_content()
   end
 
-  defp calculate_position(commands) do
-    %{depth: depth, horizontal_position: horizontal_position} = parse_commands(commands)
+  defp calculate_position(commands, opts \\ []) do
+    %{depth: depth, horizontal_position: horizontal_position} = parse_commands(commands, opts)
 
     depth * horizontal_position
   end
 
-  defp parse_commands(commands) do
+  defp parse_commands(commands, with_aim: true) do
+    state = %{depth: 0, horizontal_position: 0, aim: 0}
+
+    Enum.reduce(commands, state, &compute_command_with_aim/2)
+  end
+
+  defp parse_commands(commands, _opts) do
     state = %{depth: 0, horizontal_position: 0}
 
     Enum.reduce(commands, state, &compute_command/2)
@@ -50,5 +60,32 @@ defmodule Day2 do
   defp compute_command("up " <> amount, state) do
     amount = Util.safe_to_integer(amount, 0)
     %{state | depth: state.depth - amount}
+  end
+
+  defp compute_command_with_aim("forward " <> amount, state) do
+    amount = Util.safe_to_integer(amount, 0)
+
+    new_depth = calculate_depth(amount, state)
+
+    %{state | horizontal_position: state.horizontal_position + amount, depth: new_depth}
+  end
+
+  defp compute_command_with_aim("down " <> amount, state) do
+    amount = Util.safe_to_integer(amount, 0)
+
+    %{state | aim: state.aim + amount}
+  end
+
+  defp compute_command_with_aim("up " <> amount, state) do
+    amount = Util.safe_to_integer(amount, 0)
+
+    %{state | aim: state.aim - amount}
+  end
+
+  defp calculate_depth(forward_amount, %{
+         depth: current_depth,
+         aim: current_aim
+       }) do
+    current_depth + forward_amount * current_aim
   end
 end
--- apps/day2/test/day2_test.exs
+++ apps/day2/test/day2_test.exs
@@ -21,4 +21,25 @@ defmodule Day2Test do
       assert 2_102_357 == Day2.solve(part: 1, file_path: file_path)
     end
   end
+
+  describe "solve part 2" do
+    test "base case" do
+      input = [
+        "forward 5",
+        "down 5",
+        "forward 8",
+        "up 3",
+        "down 8",
+        "forward 2"
+      ]
+
+      assert 900 == Day2.solve(part: 2, commands: input)
+    end
+
+    test "challange" do
+      file_path = Path.join(File.cwd!(), "input.txt")
+
+      assert 2_101_031_224 == Day2.solve(part: 2, file_path: file_path)
+    end
+  end
 end
