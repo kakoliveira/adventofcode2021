22a4a53d85a337afeed559e08926d0b4de6f90c9
Carlos Oliveira <carlos.oliveira@coverflex.com>
2021-12-03 15:15:44 +0000

    Day3 part 1 solution

--- apps/day3/lib/day3.ex
+++ apps/day3/lib/day3.ex
@@ -2,4 +2,73 @@ defmodule Day3 do
   @moduledoc """
   Day 3 puzzle solutions
   """
+
+  @doc """
+      * `:part` - Possible values: `1` or `2`. Represents both parts of the puzzle.
+      * `:file_path` - When defined, the diagnostic report is read from this file instead of the next option.
+      * `:diagnostic_report` - List of binay numbers.
+  """
+  @spec solve(Keyword.t()) :: integer()
+  def solve(part: part, file_path: file_path) do
+    diagnostic_report = read_diagnostic_report(file_path)
+
+    solve(part: part, diagnostic_report: diagnostic_report)
+  end
+
+  def solve(part: 1, diagnostic_report: diagnostic_report) do
+    calculate_power_consumption(diagnostic_report)
+  end
+
+  defp read_diagnostic_report(file_path) do
+    file_path
+    |> FileReader.read()
+    |> FileReader.clean_content()
+  end
+
+  defp calculate_power_consumption(diagnostic_report) do
+    %{gamma_rate: gamma_rate, epsilon_rate: epsilon_rate} = calculate_rates(diagnostic_report)
+
+    integer_from_binary(gamma_rate) * integer_from_binary(epsilon_rate)
+  end
+
+  defp calculate_rates(diagnostic_report) do
+    state = %{gamma_rate: "", epsilon_rate: ""}
+
+    binary_matrix = Enum.map(diagnostic_report, &String.split(&1, "", trim: true))
+
+    last_column_index = get_last_column_index(binary_matrix)
+
+    Enum.reduce(0..last_column_index, state, fn column, state ->
+      %{"0" => num_zeros, "1" => num_ones} =
+        binary_matrix
+        |> fetch_column(column)
+        |> Enum.frequencies()
+
+      update_state(state, num_zeros, num_ones)
+    end)
+  end
+
+  defp get_last_column_index(binary_matrix) do
+    binary_matrix
+    |> List.first()
+    |> length()
+    |> Kernel.-(1)
+  end
+
+  defp fetch_column(matrix, column) do
+    Enum.map(matrix, &Enum.at(&1, column))
+  end
+
+  defp update_state(state, num_zeros, num_ones) do
+    if num_zeros > num_ones do
+      %{state | gamma_rate: "#{state.gamma_rate}0", epsilon_rate: "#{state.epsilon_rate}1"}
+    else
+      %{state | gamma_rate: "#{state.gamma_rate}1", epsilon_rate: "#{state.epsilon_rate}0"}
+    end
+  end
+
+  defp integer_from_binary(binary) do
+    {decimal_integer, _} = Integer.parse(binary, 2)
+    decimal_integer
+  end
 end
--- apps/day3/test/day3_test.exs
+++ apps/day3/test/day3_test.exs
@@ -1,3 +1,30 @@
 defmodule Day3Test do
   use ExUnit.Case, async: true
+
+  describe "solve part 1" do
+    test "base case" do
+      input = [
+        "00100",
+        "11110",
+        "10110",
+        "10111",
+        "10101",
+        "01111",
+        "00111",
+        "11100",
+        "10000",
+        "11001",
+        "00010",
+        "01010"
+      ]
+
+      assert 198 == Day3.solve(part: 1, diagnostic_report: input)
+    end
+
+    test "challange" do
+      file_path = Path.join(File.cwd!(), "input.txt")
+
+      assert 3_882_564 == Day3.solve(part: 1, file_path: file_path)
+    end
+  end
 end
