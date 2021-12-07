d8d9b216e61b341a1b1d38257bb72d0221f7415b
Carlos Oliveira <carlos.oliveira@coverflex.com>
2021-12-07 12:03:40 +0000

    Day7 part 1 solution

--- apps/day7/lib/day7.ex
+++ apps/day7/lib/day7.ex
@@ -2,4 +2,60 @@ defmodule Day7 do
   @moduledoc """
   Day 7 puzzle solutions
   """
+
+  @doc """
+      * `:part` - Possible values: `1` or `2`. Represents both parts of the puzzle.
+      * `:file_path` - When defined, crab positions are read from this file instead of the next option.
+      * `:crab_positions` - List of crab horizontal positions.
+  """
+  @spec solve(Keyword.t()) :: integer()
+  def solve(part: part, file_path: file_path) do
+    crab_positions = read_crab_positions(file_path)
+
+    solve(part: part, crab_positions: crab_positions)
+  end
+
+  def solve(part: 1, crab_positions: crab_positions) do
+    calculate_fuel_usage(crab_positions)
+  end
+
+  defp read_crab_positions(file_path) do
+    file_path
+    |> FileReader.read()
+    |> List.first()
+    |> String.split(",", trim: true)
+    |> Enum.map(&Util.safe_to_integer/1)
+    |> FileReader.clean_content()
+  end
+
+  defp calculate_fuel_usage(crab_positions) do
+    crab_positions
+    |> calculate_best_horizontal_position()
+    |> then(&calculate_fuel_usage(crab_positions, &1))
+  end
+
+  defp calculate_best_horizontal_position(crab_positions) do
+    positions = Enum.sort(crab_positions)
+
+    calculate_median(positions, length(positions))
+  end
+
+  defp calculate_median(positions, sample_size) when rem(sample_size, 2) == 0 do
+    mid_point = sample_size / 2
+
+    Enum.at(positions, floor(mid_point))
+  end
+
+  defp calculate_median(positions, sample_size) do
+    mid_point = sample_size / 2
+
+    (Enum.at(positions, floor(mid_point)) +
+       Enum.at(positions, ceil(mid_point))) / 2
+  end
+
+  defp calculate_fuel_usage(crab_positions, target_position) do
+    Enum.reduce(crab_positions, 0, fn crab_position, fuel ->
+      abs(crab_position - target_position) + fuel
+    end)
+  end
 end
--- apps/day7/test/day7_test.exs
+++ apps/day7/test/day7_test.exs
@@ -1,3 +1,17 @@
 defmodule Day7Test do
   use ExUnit.Case, async: true
+
+  describe "solve part 1" do
+    test "base case" do
+      input = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
+
+      assert 37 == Day7.solve(part: 1, crab_positions: input)
+    end
+
+    test "challange" do
+      file_path = Path.join(File.cwd!(), "input.txt")
+
+      assert 328_262 == Day7.solve(part: 1, file_path: file_path)
+    end
+  end
 end
