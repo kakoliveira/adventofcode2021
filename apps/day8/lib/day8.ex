4141a38871755d874766870aadcdeabf9129e964
Carlos Oliveira <carlos.oliveira@coverflex.com>
2021-12-08 18:50:20 +0000

    Day8 part 2 solution

--- apps/day8/lib/day8.ex
+++ apps/day8/lib/day8.ex
@@ -3,6 +3,40 @@ defmodule Day8 do
   Day 8 puzzle solutions
   """
 
+  @top_display_segment :top
+  @top_left_display_segment :top_left
+  @top_right_display_segment :top_right
+  @middle_display_segment :middle
+  @bottom_left_display_segment :bottom_left
+  @bottom_right_display_segment :bottom_right
+  @bottom_display_segment :bottom
+
+  @num_configurations 7
+
+  @initial_configuration [
+    {[], @top_display_segment},
+    {[], @top_left_display_segment},
+    {[], @top_right_display_segment},
+    {[], @middle_display_segment},
+    {[], @bottom_left_display_segment},
+    {[], @bottom_right_display_segment},
+    {[], @bottom_display_segment}
+  ]
+
+  @length_5_criteria :length5
+  @length_6_criteria :length6
+  @all_criterias [@length_5_criteria, @length_6_criteria]
+
+  # Guards
+  defguard is_digit_1?(segment) when byte_size(segment) == 2
+  defguard is_digit_4?(segment) when byte_size(segment) == 4
+  defguard is_digit_7?(segment) when byte_size(segment) == 3
+  defguard is_digit_8?(segment) when byte_size(segment) == 7
+
+  defguard is_unique_segment?(segment)
+           when is_digit_1?(segment) or is_digit_4?(segment) or is_digit_7?(segment) or
+                  is_digit_8?(segment)
+
   @doc """
       * `:part` - Possible values: `1` or `2`. Represents both parts of the puzzle.
       * `:file_path` - When defined, signal patterns are read from this file instead of the next option.
@@ -19,6 +53,13 @@ defmodule Day8 do
     signal_patterns
     |> parse_signal_patterns()
     |> count_unique_number_of_output_segments()
+  end
+
+  def solve(part: 2, signal_patterns: signal_patterns) do
+    signal_patterns
+    |> parse_signal_patterns()
+    |> Enum.map(&decode_signal/1)
+    |> Enum.sum()
   end
 
   defp read_signal_patterns(file_path) do
@@ -48,23 +89,304 @@ defmodule Day8 do
     end)
   end
 
-  defp is_unique_segment?(segment) do
-    is_digit_1?(segment) or is_digit_4?(segment) or is_digit_7?(segment) or is_digit_8?(segment)
+  defp decode_signal({_pattern, output} = signal_pattern) do
+    signal_pattern
+    |> deduce_configuration()
+    |> then(&decobe_output(output, &1))
   end
 
-  defp is_digit_1?(segment) do
-    String.length(segment) == 2
+  defp deduce_configuration({pattern, output}) do
+    {unique_segments, remaining_segments} =
+      Enum.split_with(pattern ++ output, &is_unique_segment?/1)
+
+    @initial_configuration
+    |> apply_unique_segments(unique_segments)
+    |> deduce_configuration(remaining_segments)
   end
 
-  defp is_digit_4?(segment) do
-    String.length(segment) == 4
+  defp deduce_configuration(configuration, remaining_segments) do
+    if all_configs_are_set?(configuration) do
+      configuration
+    else
+      recursive_configuration_deduction(configuration, remaining_segments)
+    end
   end
 
-  defp is_digit_7?(segment) do
-    String.length(segment) == 3
+  defp all_configs_are_set?(configuration) do
+    Enum.all?(configuration, fn {option, _key} ->
+      is_binary(option)
+    end)
   end
 
-  defp is_digit_8?(segment) do
-    String.length(segment) == 7
+  defp recursive_configuration_deduction(configuration, remaining_segments) do
+    configuration
+    |> Enum.sort_by(
+      fn {possibilities, key} ->
+        if is_list(possibilities) do
+          {length(possibilities), key}
+        else
+          # this one is picked, move it to the end
+          {@num_configurations + 1, key}
+        end
+      end,
+      fn {length1, key1}, {length2, key2} ->
+        if length1 == length2 do
+          sort_config_keys(key1, key2)
+        else
+          length1 <= length2
+        end
+      end
+    )
+    |> fix_possibility(remaining_segments)
+    |> deduce_configuration(remaining_segments)
+  end
+
+  defp fix_possibility([{possibilities, key} | _rest] = configuration, remaining_segments) do
+    fixed_possibility = pick_possibility(possibilities, key, remaining_segments)
+
+    configuration
+    |> List.keyreplace(key, 1, {fixed_possibility, key})
+    |> Enum.map(&reject_possibility(&1, fixed_possibility))
+  end
+
+  # top -> all others must have the possibility || first
+  # top_right -> first
+  # top_left -> all 6 length must have it || first
+  # middle -> all 5 length must have it || first
+  # bottom_left -> first
+  # bottom_right -> all 6 length must have it || first
+  # bottom -> all others must have it || first
+  # this implies an order -> middle -> top_left, bottom_right -> top, bottom -> top_right, bottom
+  defp sort_config_keys(key1, key2) do
+    key1 <= key2
+  end
+
+  defp pick_possibility(possibilities, key, remaining_segments)
+       when key in [@top_display_segment, @bottom_display_segment] do
+    filter_possibilities(possibilities, @all_criterias, remaining_segments)
+  end
+
+  defp pick_possibility(possibilities, @middle_display_segment, remaining_segments) do
+    filter_possibilities(possibilities, [@length_5_criteria], remaining_segments)
+  end
+
+  defp pick_possibility(possibilities, key, remaining_segments)
+       when key in [@top_left_display_segment, @bottom_right_display_segment] do
+    filter_possibilities(possibilities, [@length_6_criteria], remaining_segments)
+  end
+
+  defp pick_possibility(possibilities, _key, _remaining_segments) do
+    List.first(possibilities)
+  end
+
+  defp filter_possibilities([possibility], _criteria_list, _remaining_segments),
+    do: possibility
+
+  defp filter_possibilities(possibilities, criteria_list, remaining_segments) do
+    Enum.reduce(criteria_list, possibilities, fn criteria, possibilities ->
+      Enum.filter(possibilities, fn possibility ->
+        case criteria do
+          @length_5_criteria ->
+            all_remaining_segments?(remaining_segments, 5, possibility)
+
+          @length_6_criteria ->
+            all_remaining_segments?(remaining_segments, 6, possibility)
+
+          _ ->
+            true
+        end
+      end)
+    end)
+    |> List.first()
+  end
+
+  defp all_remaining_segments?(remaining_segments, target_length, possibility) do
+    remaining_segments
+    |> Enum.filter(&(String.length(&1) == target_length))
+    |> Enum.all?(&String.contains?(&1, possibility))
+  end
+
+  defp reject_possibility({possibilities, key}, fixed_possibility) when is_list(possibilities) do
+    {Enum.reject(possibilities, &(&1 == fixed_possibility)), key}
+  end
+
+  defp reject_possibility(config_item, _fixed_possibility), do: config_item
+
+  defp apply_unique_segments(configuration, unique_segments) do
+    Enum.reduce(unique_segments, configuration, fn unique_segment, config ->
+      unique_segment
+      |> String.split("", trim: true)
+      |> then(&add_config_possibilities(config, &1, unique_segment))
+    end)
+  end
+
+  defp add_config_possibilities(configuration, possibilities, unique_segment)
+       when is_digit_1?(unique_segment) do
+    configuration
+    |> set_possibilities_for_display_segment(@top_right_display_segment, possibilities)
+    |> set_possibilities_for_display_segment(@bottom_right_display_segment, possibilities)
+  end
+
+  defp add_config_possibilities(configuration, possibilities, unique_segment)
+       when is_digit_4?(unique_segment) do
+    configuration
+    |> set_possibilities_for_display_segment(@top_right_display_segment, possibilities)
+    |> set_possibilities_for_display_segment(@top_left_display_segment, possibilities)
+    |> set_possibilities_for_display_segment(@middle_display_segment, possibilities)
+    |> set_possibilities_for_display_segment(@bottom_right_display_segment, possibilities)
+  end
+
+  defp add_config_possibilities(configuration, possibilities, unique_segment)
+       when is_digit_7?(unique_segment) do
+    configuration
+    |> set_possibilities_for_display_segment(@top_display_segment, possibilities)
+    |> set_possibilities_for_display_segment(@top_right_display_segment, possibilities)
+    |> set_possibilities_for_display_segment(@bottom_right_display_segment, possibilities)
+  end
+
+  defp add_config_possibilities(configuration, possibilities, unique_segment)
+       when is_digit_8?(unique_segment) do
+    configuration
+    |> set_possibilities_for_display_segment(@middle_display_segment, possibilities)
+    |> set_possibilities_for_display_segment(@top_display_segment, possibilities)
+    |> set_possibilities_for_display_segment(@top_right_display_segment, possibilities)
+    |> set_possibilities_for_display_segment(@top_left_display_segment, possibilities)
+    |> set_possibilities_for_display_segment(@bottom_right_display_segment, possibilities)
+    |> set_possibilities_for_display_segment(@bottom_left_display_segment, possibilities)
+    |> set_possibilities_for_display_segment(@bottom_display_segment, possibilities)
+  end
+
+  defp add_config_possibilities(configuration, _possibilities, _unique_segment), do: configuration
+
+  defp set_possibilities_for_display_segment(configuration, key, possibilities) do
+    {current_possibilities, ^key} = List.keyfind(configuration, key, 1)
+
+    set_possibilities_for_display_segment(
+      configuration,
+      key,
+      current_possibilities,
+      possibilities
+    )
+  end
+
+  defp set_possibilities_for_display_segment(configuration, key, [], possibilities) do
+    List.keyreplace(configuration, key, 1, {possibilities, key})
+  end
+
+  defp set_possibilities_for_display_segment(
+         configuration,
+         _key,
+         [current_possibility],
+         possibilities
+       ) do
+    if current_possibility in possibilities do
+      configuration
+    else
+      raise ArgumentError
+    end
+  end
+
+  defp set_possibilities_for_display_segment(
+         configuration,
+         key,
+         current_possibilities,
+         possibilities
+       ) do
+    remaining_possibilities = Enum.reject(current_possibilities, &(&1 not in possibilities))
+
+    List.keyreplace(configuration, key, 1, {remaining_possibilities, key})
+  end
+
+  def decobe_output(output, configuration) when length(configuration) == @num_configurations do
+    output
+    |> Enum.map(&decode_digit(&1, configuration))
+    |> Enum.join()
+    |> String.to_integer()
+  end
+
+  defp decode_digit(digit_segment, configuration) when is_binary(digit_segment) do
+    if is_unique_segment?(digit_segment) do
+      decode_unique_digit(digit_segment)
+    else
+      digit_segment
+      |> String.split("", trim: true)
+      |> decode_digit(configuration)
+    end
+  end
+
+  defp decode_digit(digit_signals, configuration) do
+    digit_signals
+    |> Enum.reduce([], fn signal, acc ->
+      configuration
+      |> List.keyfind(signal, 0)
+      |> elem(1)
+      |> List.wrap()
+      |> Kernel.++(acc)
+    end)
+    |> interpret_display()
+  end
+
+  defp decode_unique_digit(unique_digit_segment) when is_digit_1?(unique_digit_segment), do: 1
+  defp decode_unique_digit(unique_digit_segment) when is_digit_4?(unique_digit_segment), do: 4
+  defp decode_unique_digit(unique_digit_segment) when is_digit_7?(unique_digit_segment), do: 7
+  defp decode_unique_digit(unique_digit_segment) when is_digit_8?(unique_digit_segment), do: 8
+  defp decode_unique_digit(_unique_digit_segment), do: raise(ArgumentError)
+
+  defp interpret_display(active_display_segments) do
+    cond do
+      is_digit_0?(active_display_segments) ->
+        0
+
+      is_digit_2?(active_display_segments) ->
+        2
+
+      is_digit_3?(active_display_segments) ->
+        3
+
+      is_digit_5?(active_display_segments) ->
+        5
+
+      is_digit_6?(active_display_segments) ->
+        6
+
+      is_digit_9?(active_display_segments) ->
+        9
+
+      true ->
+        raise ArgumentError
+    end
+  end
+
+  defp is_digit_0?(active_display_segments) do
+    length(active_display_segments) == 6 and
+      @middle_display_segment not in active_display_segments
+  end
+
+  defp is_digit_2?(active_display_segments) do
+    length(active_display_segments) == 5 and
+      @top_left_display_segment not in active_display_segments and
+      @bottom_right_display_segment not in active_display_segments
+  end
+
+  defp is_digit_3?(active_display_segments) do
+    length(active_display_segments) == 5 and
+      @top_left_display_segment not in active_display_segments and
+      @bottom_left_display_segment not in active_display_segments
+  end
+
+  defp is_digit_5?(active_display_segments) do
+    length(active_display_segments) == 5 and
+      @top_right_display_segment not in active_display_segments and
+      @bottom_left_display_segment not in active_display_segments
+  end
+
+  defp is_digit_6?(active_display_segments) do
+    length(active_display_segments) == 6 and
+      @top_right_display_segment not in active_display_segments
+  end
+
+  defp is_digit_9?(active_display_segments) do
+    length(active_display_segments) == 6 and
+      @bottom_left_display_segment not in active_display_segments
   end
 end
--- apps/day8/test/day8_test.exs
+++ apps/day8/test/day8_test.exs
@@ -25,4 +25,53 @@ defmodule Day8Test do
       assert 288 == Day8.solve(part: 1, file_path: file_path)
     end
   end
+
+  describe "solve part 2" do
+    test "simpler case" do
+      input = [
+        "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"
+      ]
+
+      assert 5353 == Day8.solve(part: 2, signal_patterns: input)
+    end
+
+    test "base case" do
+      input = [
+        "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe",
+        "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc",
+        "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg",
+        "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb",
+        "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea",
+        "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb",
+        "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe",
+        "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef",
+        "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb",
+        "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"
+      ]
+
+      assert 61229 == Day8.solve(part: 2, signal_patterns: input)
+    end
+
+    test "challange" do
+      file_path = Path.join(File.cwd!(), "input.txt")
+
+      assert 940_724 == Day8.solve(part: 2, file_path: file_path)
+    end
+  end
+
+  describe "decobe_output/2" do
+    test "examples" do
+      configuration = [
+        {"d", :top},
+        {"e", :top_left},
+        {"a", :top_right},
+        {"f", :middle},
+        {"g", :bottom_left},
+        {"b", :bottom_right},
+        {"c", :bottom}
+      ]
+
+      assert 5353 == Day8.decobe_output(["cdfeb", "fcadb", "cdfeb", "cdbaf"], configuration)
+    end
+  end
 end
