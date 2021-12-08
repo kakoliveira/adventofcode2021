1032768c37f5ad9a70063f61e31c105b112b3a1e
Carlos Oliveira <carlos.oliveira@coverflex.com>
2021-12-08 15:43:15 +0000

    Day8 part 1 solution

--- apps/day8/lib/day8.ex
+++ apps/day8/lib/day8.ex
@@ -2,4 +2,69 @@ defmodule Day8 do
   @moduledoc """
   Day 8 puzzle solutions
   """
+
+  @doc """
+      * `:part` - Possible values: `1` or `2`. Represents both parts of the puzzle.
+      * `:file_path` - When defined, signal patterns are read from this file instead of the next option.
+      * `:signal_patterns` - List of signal patterns.
+  """
+  @spec solve(Keyword.t()) :: integer()
+  def solve(part: part, file_path: file_path) do
+    signal_patterns = read_signal_patterns(file_path)
+
+    solve(part: part, signal_patterns: signal_patterns)
+  end
+
+  def solve(part: 1, signal_patterns: signal_patterns) do
+    signal_patterns
+    |> parse_signal_patterns()
+    |> count_unique_number_of_output_segments()
+  end
+
+  defp read_signal_patterns(file_path) do
+    file_path
+    |> FileReader.read()
+    |> FileReader.clean_content()
+  end
+
+  defp parse_signal_patterns(signal_patterns) do
+    Enum.map(signal_patterns, &parse_signal_pattern/1)
+  end
+
+  defp parse_signal_pattern(signal_pattern) do
+    [signal, output] =
+      signal_pattern
+      |> String.split(" | ", trim: true)
+      |> Enum.map(&String.split(&1, " ", trim: true))
+
+    {signal, output}
+  end
+
+  defp count_unique_number_of_output_segments(signal_patterns) do
+    signal_patterns
+    |> Enum.map(&elem(&1, 1))
+    |> Enum.reduce(0, fn output_segment, count ->
+      count + Enum.count(output_segment, &is_unique_segment?/1)
+    end)
+  end
+
+  defp is_unique_segment?(segment) do
+    is_digit_1?(segment) or is_digit_4?(segment) or is_digit_7?(segment) or is_digit_8?(segment)
+  end
+
+  defp is_digit_1?(segment) do
+    String.length(segment) == 2
+  end
+
+  defp is_digit_4?(segment) do
+    String.length(segment) == 4
+  end
+
+  defp is_digit_7?(segment) do
+    String.length(segment) == 3
+  end
+
+  defp is_digit_8?(segment) do
+    String.length(segment) == 7
+  end
 end
--- apps/day8/test/day8_test.exs
+++ apps/day8/test/day8_test.exs
@@ -1,3 +1,28 @@
 defmodule Day8Test do
   use ExUnit.Case, async: true
+
+  describe "solve part 1" do
+    test "base case" do
+      input = [
+        "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe",
+        "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc",
+        "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg",
+        "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb",
+        "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea",
+        "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb",
+        "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe",
+        "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef",
+        "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb",
+        "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"
+      ]
+
+      assert 26 == Day8.solve(part: 1, signal_patterns: input)
+    end
+
+    test "challange" do
+      file_path = Path.join(File.cwd!(), "input.txt")
+
+      assert 288 == Day8.solve(part: 1, file_path: file_path)
+    end
+  end
 end
