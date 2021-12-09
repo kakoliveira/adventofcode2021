820d7214e8fc2a79021c145807a009fe0198d11c
Carlos Oliveira <carlos.oliveira@coverflex.com>
2021-12-09 13:23:40 +0000

    Day9 part 1 solution

--- apps/day9/lib/day9.ex
+++ apps/day9/lib/day9.ex
@@ -2,4 +2,124 @@ defmodule Day9 do
   @moduledoc """
   Day 9 puzzle solutions
   """
+
+  @doc """
+      * `:part` - Possible values: `1` or `2`. Represents both parts of the puzzle.
+      * `:file_path` - When defined, the heightmap are read from this file instead of the next option.
+      * `:heightmap` - Heightmap matrix.
+  """
+  @spec solve(Keyword.t()) :: integer()
+  def solve(part: part, file_path: file_path) do
+    heightmap = read_heightmap(file_path)
+
+    solve(part: part, heightmap: heightmap)
+  end
+
+  def solve(part: 1, heightmap: heightmap) do
+    heightmap
+    |> parse_matrix()
+    |> find_low_points()
+    |> calculate_risk_level()
+  end
+
+  defp read_heightmap(file_path) do
+    file_path
+    |> FileReader.read()
+    |> FileReader.clean_content()
+  end
+
+  defp parse_matrix(heightmap) do
+    Enum.map(heightmap, &parse_matrix_row/1)
+  end
+
+  defp parse_matrix_row(matrix_row) do
+    matrix_row
+    |> String.split("", trim: true)
+    |> Enum.map(&Util.safe_to_integer/1)
+  end
+
+  defp find_low_points(heightmap_matrix) do
+    num_columns = Util.matrix_column_size(heightmap_matrix)
+
+    num_rows = length(heightmap_matrix)
+
+    heightmap_matrix
+    |> Enum.with_index()
+    |> Enum.reduce([], fn row, low_points ->
+      find_low_points(row, {heightmap_matrix, num_rows, num_columns}) ++ low_points
+    end)
+  end
+
+  defp find_low_points({row, row_index}, heightmap_matrix_struct) do
+    row
+    |> Enum.with_index()
+    |> Enum.reduce([], fn {height, column_index}, low_points ->
+      if is_low_point?(
+           height,
+           row,
+           {row_index, column_index},
+           heightmap_matrix_struct
+         ) do
+        [height] ++ low_points
+      else
+        low_points
+      end
+    end)
+  end
+
+  defp is_low_point?(
+         height,
+         row,
+         {row_index, column_index},
+         {heightmap_matrix, num_rows, num_columns}
+       ) do
+    less_then_left?(height, column_index, row) and
+      less_than_right?(height, column_index, row, num_columns) and
+      less_than_bottom?(height, column_index, row_index, heightmap_matrix, num_rows) and
+      less_than_top?(height, column_index, row_index, heightmap_matrix, num_rows)
+  end
+
+  defp less_then_left?(_height, 0, _row), do: true
+
+  defp less_then_left?(height, column_index, row) do
+    height < Enum.at(row, column_index - 1)
+  end
+
+  defp less_than_right?(_height, column_index, _row, num_columns)
+       when column_index == num_columns - 1,
+       do: true
+
+  defp less_than_right?(height, column_index, row, _num_columns) do
+    height < Enum.at(row, column_index + 1)
+  end
+
+  defp less_than_bottom?(_height, _column_index, row_index, _heightmap_matrix, num_rows)
+       when row_index == num_rows - 1,
+       do: true
+
+  defp less_than_bottom?(height, column_index, row_index, heightmap_matrix, _num_rows) do
+    value_bellow =
+      heightmap_matrix
+      |> Enum.at(row_index + 1)
+      |> Enum.at(column_index)
+
+    height < value_bellow
+  end
+
+  defp less_than_top?(_height, _column_index, 0, _heightmap_matrix, _num_rows), do: true
+
+  defp less_than_top?(height, column_index, row_index, heightmap_matrix, _num_rows) do
+    value_above =
+      heightmap_matrix
+      |> Enum.at(row_index - 1)
+      |> Enum.at(column_index)
+
+    height < value_above
+  end
+
+  defp calculate_risk_level(low_points) do
+    low_points
+    |> Enum.sum()
+    |> Kernel.+(length(low_points))
+  end
 end
--- apps/day9/test/day9_test.exs
+++ apps/day9/test/day9_test.exs
@@ -1,3 +1,23 @@
 defmodule Day9Test do
   use ExUnit.Case, async: true
+
+  describe "solve part 1" do
+    test "base case" do
+      input = [
+        "2199943210",
+        "3987894921",
+        "9856789892",
+        "8767896789",
+        "9899965678"
+      ]
+
+      assert 15 == Day9.solve(part: 1, heightmap: input)
+    end
+
+    test "challange" do
+      file_path = Path.join(File.cwd!(), "input.txt")
+
+      assert 489 == Day9.solve(part: 1, file_path: file_path)
+    end
+  end
 end
