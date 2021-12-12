95e864b4b7d9ace2549a50100e84d03d9b7fdcfb
Carlos Oliveira <carlos.oliveira@coverflex.com>
2021-12-12 15:05:44 +0000

    Day12 part 1 solution

--- apps/day12/lib/day12.ex
+++ apps/day12/lib/day12.ex
@@ -2,4 +2,71 @@ defmodule Day12 do
   @moduledoc """
   Day 12 puzzle solutions
   """
+
+  @start_node "start"
+  @end_node "end"
+
+  @doc """
+      * `:part` - Possible values: `1` or `2`. Represents both parts of the puzzle.
+      * `:file_path` - When defined, the graph edges are read from this file instead of the next option.
+      * `:edges` - Graph edges.
+  """
+  @spec solve(Keyword.t()) :: integer()
+  def solve(part: part, file_path: file_path) do
+    edges = read_edges(file_path)
+
+    solve(part: part, edges: edges)
+  end
+
+  def solve(part: 1, edges: edges) do
+    edges
+    |> build_graph()
+    |> find_all_paths()
+    |> Enum.count()
+  end
+
+  defp read_edges(file_path) do
+    file_path
+    |> FileReader.read()
+    |> FileReader.clean_content()
+  end
+
+  defp build_graph(edges) do
+    edges
+    |> Enum.map(fn edge ->
+      [from, to] = String.split(edge, "-", trim: true)
+
+      [{from, to}, {to, from}]
+    end)
+    |> List.flatten()
+  end
+
+  defp find_all_paths(edges) do
+    traverse(@start_node, [], edges)
+  end
+
+  defp traverse(@end_node, visited_nodes, _edges), do: [[@end_node] ++ visited_nodes]
+
+  defp traverse(current_node, visited_nodes, edges) do
+    current_node
+    |> find_all_next_nodes(visited_nodes, edges)
+    |> Enum.reduce([], fn next_node, paths ->
+      traverse(next_node, [current_node] ++ visited_nodes, edges) ++ paths
+    end)
+  end
+
+  defp find_all_next_nodes(node, visited_nodes, edges) do
+    edges
+    |> Enum.filter(&(elem(&1, 0) == node))
+    |> Enum.map(&elem(&1, 1))
+    |> Enum.filter(&can_move_into?(&1, visited_nodes))
+  end
+
+  defp can_move_into?(next_node, visited_nodes) do
+    next_node not in visited_nodes or not is_small_cave?(next_node)
+  end
+
+  defp is_small_cave?(next_node) do
+    next_node == String.downcase(next_node)
+  end
 end
--- apps/day12/test/day12_test.exs
+++ apps/day12/test/day12_test.exs
@@ -1,3 +1,67 @@
 defmodule Day12Test do
   use ExUnit.Case, async: true
+
+  describe "solve part 1" do
+    test "base case" do
+      input = [
+        "start-A",
+        "start-b",
+        "A-c",
+        "A-b",
+        "b-d",
+        "A-end",
+        "b-end"
+      ]
+
+      assert 10 == Day12.solve(part: 1, edges: input)
+    end
+
+    test "larger base case" do
+      input = [
+        "dc-end",
+        "HN-start",
+        "start-kj",
+        "dc-start",
+        "dc-HN",
+        "LN-dc",
+        "HN-end",
+        "kj-sa",
+        "kj-HN",
+        "kj-dc"
+      ]
+
+      assert 19 == Day12.solve(part: 1, edges: input)
+    end
+
+    test "another larger base case" do
+      input = [
+        "fs-end",
+        "he-DX",
+        "fs-he",
+        "start-DX",
+        "pj-DX",
+        "end-zg",
+        "zg-sl",
+        "zg-pj",
+        "pj-he",
+        "RW-he",
+        "fs-DX",
+        "pj-RW",
+        "zg-RW",
+        "start-pj",
+        "he-WI",
+        "zg-he",
+        "pj-fs",
+        "start-RW"
+      ]
+
+      assert 226 == Day12.solve(part: 1, edges: input)
+    end
+
+    test "challange" do
+      file_path = Path.join(File.cwd!(), "input.txt")
+
+      assert 3298 == Day12.solve(part: 1, file_path: file_path)
+    end
+  end
 end
