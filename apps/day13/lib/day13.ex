22ed8887320b02df26f3adcd5e490da1268ddbb5
Carlos Oliveira <carlos.oliveira@coverflex.com>
2021-12-13 20:36:21 +0000

    Day13 part 1 solution

--- apps/day13/lib/day13.ex
+++ apps/day13/lib/day13.ex
@@ -2,4 +2,91 @@ defmodule Day13 do
   @moduledoc """
   Day 13 puzzle solutions
   """
+
+  @doc """
+      * `:part` - Possible values: `1` or `2`. Represents both parts of the puzzle.
+      * `:file_path` - When defined, sheet is read from this file instead of the next option.
+      * `:sheet` - Transparent sheet.
+  """
+  @spec solve(Keyword.t()) :: integer()
+  def solve(part: part, file_path: file_path) do
+    sheet = read_sheet(file_path)
+
+    solve(part: part, sheet: sheet)
+  end
+
+  def solve(part: 1, sheet: sheet) do
+    %{points: points, folds: folds} = parse_sheet(sheet)
+
+    folds
+    |> List.first()
+    |> apply_fold(points)
+    |> Enum.count()
+  end
+
+  defp read_sheet(file_path) do
+    file_path
+    |> FileReader.read()
+    |> FileReader.clean_content()
+  end
+
+  defp parse_sheet(sheet) do
+    state = %{points: %{}, folds: []}
+
+    sheet
+    |> Enum.reduce(state, &parse_line/2)
+    |> Map.update!(:folds, &Enum.reverse/1)
+  end
+
+  defp parse_line("fold along " <> fold, state) do
+    Map.update!(state, :folds, fn folds ->
+      [parse_fold(fold)] ++ folds
+    end)
+  end
+
+  defp parse_line(point, state) do
+    Map.update!(state, :points, fn points ->
+      Map.put(points, parse_point(point), true)
+    end)
+  end
+
+  defp parse_fold(fold) do
+    [axis, pivot] = String.split(fold, "=", trim: true)
+
+    {axis, Util.safe_to_integer(pivot)}
+  end
+
+  defp parse_point(point) do
+    [x, y] = String.split(point, ",", trim: true)
+
+    {Util.safe_to_integer(x), Util.safe_to_integer(y)}
+  end
+
+  defp apply_fold(fold, points) do
+    {points_to_fold, fixed_points} = Enum.split_with(points, &point_to_fold?(&1, fold))
+
+    points_to_fold
+    |> Enum.reduce(%{}, &fold_point(&1, fold, &2))
+    |> Map.merge(Map.new(fixed_points))
+  end
+
+  defp point_to_fold?({{x, _y}, true}, {"x", pivot}) do
+    x > pivot
+  end
+
+  defp point_to_fold?({{_x, y}, true}, {"y", pivot}) do
+    y > pivot
+  end
+
+  defp fold_point({{x, y}, true}, {"x", pivot}, new_points) do
+    Map.put(new_points, {pivot(x, pivot), y}, true)
+  end
+
+  defp fold_point({{x, y}, true}, {"y", pivot}, new_points) do
+    Map.put(new_points, {x, pivot(y, pivot)}, true)
+  end
+
+  defp pivot(coordinate, pivot) do
+    2 * pivot - coordinate
+  end
 end
--- apps/day13/test/day13_test.exs
+++ apps/day13/test/day13_test.exs
@@ -1,3 +1,38 @@
 defmodule Day13Test do
   use ExUnit.Case, async: true
+
+  describe "solve part 1" do
+    test "base case" do
+      input = [
+        "6,10",
+        "0,14",
+        "9,10",
+        "0,3",
+        "10,4",
+        "4,11",
+        "6,0",
+        "6,12",
+        "4,1",
+        "0,13",
+        "10,12",
+        "3,4",
+        "3,0",
+        "8,4",
+        "1,10",
+        "2,14",
+        "8,10",
+        "9,0",
+        "fold along y=7",
+        "fold along x=5"
+      ]
+
+      assert 17 == Day13.solve(part: 1, sheet: input)
+    end
+
+    test "challange" do
+      file_path = Path.join(File.cwd!(), "input.txt")
+
+      assert 827 == Day13.solve(part: 1, file_path: file_path)
+    end
+  end
 end
